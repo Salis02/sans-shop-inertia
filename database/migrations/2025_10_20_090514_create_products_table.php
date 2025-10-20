5eceba02032537f9ed26f63cf722e4d39e166c24
Salis02 <nizarsalis@gmail.com>
2025-10-20 16:07:20 +0700

    migration roles, categories, products, dan products image

---
+++ database/migrations/2025_10_20_090514_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('uuid')->primary();
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->string('name');
+            $table->string('slug')->unique()->index();
+            $table->text('description')->nullable();
+            $table->integer('price'); // dalam rupiah
+            $table->integer('stock')->default(0);
+            $table->string('sku')->unique();
+            $table->enum('status', ['draft', 'active', 'archived'])->default('draft');
+            $table->foreignUuid('created_by')->constrained('users', 'uuid');
+            $table->foreignUuid('updated_by')->nullable()->constrained('users', 'uuid');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
