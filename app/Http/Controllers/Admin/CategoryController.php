4ad53bbc096bd10d2514b2e571409aa1c9974b52
Salis02 <nizarsalis@gmail.com>
2025-10-28 09:19:55 +0700

    method di CategoryController

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -7,20 +7,21 @@ use App\Models\Category;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
+use Illuminate\Support\Str;
 
 class CategoryController extends Controller
 {
     public function index(Request $request): Response
     {
         $categories = Category::query()
-        ->with('parent')
-        ->withCount('products')
-        ->when($request->search, function ($query, $search){
-            $query->where('name', 'like', "%{ $search }%");
-        })
-        ->latest()
-        ->paginate(10)
-        ->withQueryString();
+            ->with('parent')
+            ->withCount('products')
+            ->when($request->search, function ($query, $search) {
+                $query->where('name', 'like', "%{ $search }%");
+            })
+            ->latest()
+            ->paginate(10)
+            ->withQueryString();
 
         return Inertia::render('Admin/Categories/Index', [
             'categories' => $categories,
@@ -28,7 +29,7 @@ class CategoryController extends Controller
         ]);
     }
 
-    public function create (): Response
+    public function create(): Response
     {
         $parentCategories = Category::whereNull('parent_id')->get();
 
@@ -37,5 +38,37 @@ class CategoryController extends Controller
         ]);
     }
 
-    
+    public function store(StoreCategoryRequest $request)
+    {
+        $category = Category::create([
+            ...$request->validate(),
+            'slug' => Str::slug($request->name),
+        ]);
+
+        return redirect()
+            ->route('admin.categories.index')
+            ->with('success', 'Kategori berhasil ditambahkan');
+    }
+
+    public function edit(Category $category): Response
+    {
+        $parentCategories = Category::whereNull('parent_id')
+            ->where('id', '!=', $category->id)
+            ->get();
+
+        return Inertia::render('Admin/Categories/Edit', [
+            'category' => $category->load('parent'),
+            'parentCategories' => $parentCategories,
+        ]);
+    }
+
+    public function update(UpdateCategoryRequest $request, Category $category) 
+    {
+        $category->update([
+            ...$request->validate(),
+            'slug' => Str::slug($request->name),
+        ]);
+
+        return redirect()
+    }
 }
